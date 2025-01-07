eaa543796875cf561d6efd9238015f9612c89e45
Balazs Letai <92532601+balazsletai@users.noreply.github.com>
2025-01-07 13:25:01 +0100

    Refactor SampleJournalEditorConfigContributor.java

--- src/main/java/sample/journal/editor/config/contributor/SampleJournalEditorConfigContributor.java
+++ src/main/java/sample/journal/editor/config/contributor/SampleJournalEditorConfigContributor.java
@@ -5,98 +5,72 @@ import com.liferay.portal.kernel.editor.configuration.BaseEditorConfigContributo
 import com.liferay.portal.kernel.json.JSONArray;
 import com.liferay.portal.kernel.json.JSONObject;
 import com.liferay.portal.kernel.json.JSONFactoryUtil;
-import com.liferay.portal.kernel.json.JSONUtil;
 import com.liferay.portal.kernel.portlet.RequestBackedPortletURLFactory;
 import com.liferay.portal.kernel.theme.ThemeDisplay;
-import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.Validator;
-
 import org.osgi.service.component.annotations.Component;
 
 import java.util.Map;
 
 /**
- * @author balazs.letai@liferay.com
+ * Editor configuration contributor for CKEditor.
+ * Author: balazs.letai@liferay.com
  */
 
 @Component(
-	immediate = true,
-	property = {
-		"editor.config.key=contentEditor", "editor.config.key=rich_text",
-
-		// For Liferay 7.3
-		"editor.config.key=translateEditor",
-		// For Liferay 7.4
-		// "editor.config.key=defaultTranslateEditor",
-
-		"editor.name=ckeditor", "editor.name=ckeditor_classic",
-		"service.ranking:Integer=100"
-	},
-	service = EditorConfigContributor.class
+    immediate = true,
+    property = {
+        "editor.config.key=contentEditor", "editor.config.key=rich_text",
+        // "editor.config.key=translateEditor", // For Liferay 7.3
+        "editor.config.key=defaultTranslateEditor", // For Liferay 7.4
+        "editor.name=ckeditor", "editor.name=ckeditor_classic",
+        "service.ranking:Integer=100"
+    },
+    service = EditorConfigContributor.class
 )
-public class SampleJournalEditorConfigContributor
-	extends BaseEditorConfigContributor {
+public class SampleJournalEditorConfigContributor extends BaseEditorConfigContributor {
 
-	@Override
-	public void populateConfigJSONObject(JSONObject jsonObject, Map<String, Object> inputEditorTaglibAttributes,
-			ThemeDisplay themeDisplay, RequestBackedPortletURLFactory requestBackedPortletURLFactory) {
+    private static final String EXTRA_PLUGINS = "maximize,showblocks,iframe,mathjax,pastefromword,justify,colorbutton,basicstyles,indent";
+    private static final String MATH_JAX_LIB_URL = "//cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.9/MathJax.js?config=TeX-MML-AM_CHTML";
+    private static final String[] EXTRA_TOOLS = {
+        "ShowBlocks", "Iframe", "Maximize", "PasteFromWord",
+        "JustifyRight", "JustifyCenter", "JustifyLeft", "JustifyBlock",
+        "TextColor", "BGColor", "Superscript", "RemoveFormat", "Strike",
+        "Subscript", "Outdent", "Indent"
+    };
 
-		jsonObject.put("pasteFromWordRemoveFontStyles", Boolean.TRUE);
-		jsonObject.put("pasteFromWordRemoveStyles", Boolean.TRUE);
+    @Override
+    public void populateConfigJSONObject(JSONObject jsonObject, Map<String, Object> inputEditorTaglibAttributes,
+                                         ThemeDisplay themeDisplay, RequestBackedPortletURLFactory requestBackedPortletURLFactory) {
 
-		String extraPlugins = jsonObject.getString("extraPlugins");
+        addExtraPlugins(jsonObject);
+        configureToolbar(jsonObject);
+    }
 
-		if (Validator.isNotNull(extraPlugins)) {
-			extraPlugins = extraPlugins + ",maximize,showblocks,iframe,mathjax,pastefromword,justify,colorbutton,basicstyles,indent";
-			jsonObject.put("extraPlugins", extraPlugins);
+    private void addExtraPlugins(JSONObject jsonObject) {
+        String extraPlugins = jsonObject.getString("extraPlugins");
 
-			JSONArray toolbarsLiferay = jsonObject.getJSONArray("toolbar_liferay");
-			JSONArray extraTools_liferay = JSONFactoryUtil.createJSONArray();
+        if (Validator.isNotNull(extraPlugins)) {
+            jsonObject.put("extraPlugins", extraPlugins + "," + EXTRA_PLUGINS);
+        }
+    }
 
-			if (Validator.isNotNull(toolbarsLiferay)) {
-				extraTools_liferay = toolbarsLiferay.getJSONArray(toolbarsLiferay.length() - 1);
-			}
+    private void configureToolbar(JSONObject jsonObject) {
+        JSONArray toolbarsLiferay = jsonObject.getJSONArray("toolbar_liferay");
+        JSONArray extraToolsLiferay = JSONFactoryUtil.createJSONArray();
 
-			extraTools_liferay.put("-");
-			extraTools_liferay.put("ShowBlocks");
-			extraTools_liferay.put("-");
-			extraTools_liferay.put("Iframe");
-			extraTools_liferay.put("-");
-			extraTools_liferay.put("Maximize");
-			extraTools_liferay.put("-");
-			extraTools_liferay.put("Mathjax");
-			extraTools_liferay.put("-");
-			extraTools_liferay.put("PasteFromWord");
-			extraTools_liferay.put("-");
-			extraTools_liferay.put("JustifyRight");
-			extraTools_liferay.put("-");
-			extraTools_liferay.put("JustifyCenter");
-			extraTools_liferay.put("-");
-			extraTools_liferay.put("JustifyLeft");
-			extraTools_liferay.put("-");
-			extraTools_liferay.put("JustifyBlock");
-			extraTools_liferay.put("-");
-			extraTools_liferay.put("TextColor");
-			extraTools_liferay.put("-");
-			extraTools_liferay.put("BGColor");
-			extraTools_liferay.put("-");
-			extraTools_liferay.put("Superscript");
-			extraTools_liferay.put("-");
-			extraTools_liferay.put("RemoveFormat");
-			extraTools_liferay.put("-");
-			extraTools_liferay.put("Strike");
-			extraTools_liferay.put("-");
-			extraTools_liferay.put("Subscript");
-			extraTools_liferay.put("-");
-			extraTools_liferay.put("Outdent");
-			extraTools_liferay.put("-");
-			extraTools_liferay.put("Indent");
-			extraTools_liferay.put("-");
+        if (Validator.isNotNull(toolbarsLiferay)) {
+            extraToolsLiferay = toolbarsLiferay.getJSONArray(toolbarsLiferay.length() - 1);
+        }
 
-			jsonObject.put("mathJaxLib", "//cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.9/MathJax.js?config=TeX-MML-AM_CHTML");
+        for (String tool : EXTRA_TOOLS) {
+            extraToolsLiferay.put("-");
+            extraToolsLiferay.put(tool);
+        }
 
-		}
-
-	}
-
+        jsonObject.put("toolbar_liferay", extraToolsLiferay);
+        jsonObject.put("pasteFromWordRemoveFontStyles", Boolean.TRUE);
+        jsonObject.put("pasteFromWordRemoveStyles", Boolean.TRUE);
+        jsonObject.put("mathJaxLib", MATH_JAX_LIB_URL);
+    }
 }
