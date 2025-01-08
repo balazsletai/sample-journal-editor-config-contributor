cd0dc3e003104288c5a0a4165154ff21bca2ce52
Balazs Letai <92532601+balazsletai@users.noreply.github.com>
2025-01-08 16:06:24 +0100

    Update SampleJournalEditorConfigContributor.java

--- src/main/java/sample/journal/editor/config/contributor/SampleJournalEditorConfigContributor.java
+++ src/main/java/sample/journal/editor/config/contributor/SampleJournalEditorConfigContributor.java
@@ -30,13 +30,12 @@ import java.util.Map;
 )
 public class SampleJournalEditorConfigContributor extends BaseEditorConfigContributor {
 
-    private static final String EXTRA_PLUGINS = "maximize,showblocks,iframe,mathjax,pastefromword,justify,colorbutton,basicstyles,indent,smiley";
+    private static final String EXTRA_PLUGINS = "showblocks,iframe,maximize,justify,colorbutton,basicstyles,indent,mathjax,smiley";
     private static final String MATH_JAX_LIB_URL = "//cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.9/MathJax.js?config=TeX-MML-AM_CHTML";
     private static final String[] EXTRA_TOOLS = {
-        "ShowBlocks", "Iframe", "Maximize", "PasteFromWord",
-        "JustifyRight", "JustifyCenter", "JustifyLeft", "JustifyBlock",
-        "TextColor", "BGColor", "Superscript", "RemoveFormat", "Strike",
-        "Subscript", "Outdent", "Indent" , "Smiley"
+        "ShowBlocks", "Iframe", "Maximize", "JustifyLeft" , "JustifyCenter", 
+        "JustifyRight" , "JustifyBlock", "TextColor", "BGColor", "RemoveFormat",
+        "Strike", "Subscript", "Superscript", "Outdent", "Indent" , "Mathjax", "Smiley"
     };
 
     @Override
@@ -69,8 +68,6 @@ public class SampleJournalEditorConfigContributor extends BaseEditorConfigContri
         }
 
         jsonObject.put("toolbar_liferay", extraToolsLiferay);
-        jsonObject.put("pasteFromWordRemoveFontStyles", Boolean.TRUE);
-        jsonObject.put("pasteFromWordRemoveStyles", Boolean.TRUE);
         jsonObject.put("mathJaxLib", MATH_JAX_LIB_URL);
     }
 }
